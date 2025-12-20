6b55c175f157940cf8319b1959008bf16211cfba
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-12-20 15:55:05 +0530

    filling api's

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from starlette import status
 from .config import firebaseConfig
 import pyrebase
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, auth
+from .schema import LoginSchema, SignUpSchema
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 app = FastAPI()
 
@@ -13,17 +17,29 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 @app.post('/signup')
-async def signup():
-  pass
+async def signup(user_data: SignUpSchema):
+  email = user_data.email
+  password = user_data.password
+  try:
+      user = auth.create_user(
+        email=email,
+        password=password
+      )
+      auth.send_email_verification(user['idToken'])
+      return JSONResponse(status_code=status.HTTP_201_CREATED,content={"message": "User created successfully", "uid": user.uid})
+  except Exception as e:
+      raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail={"message": str(e)})
 
 @app.post('/login')
-async def login():
-  pass
-
-@app.post('/logout')
-async def logout():
-  pass
-
-@app.post('ping')
-async def validate_token():
-  pass
+async def login(user_data: LoginSchema):
+  email = user_data.email
+  password = user_data.password
+  try:
+      user = firebase.auth().sign_in_with_email_and_password(
+        email=email,
+        password=password
+      )
+      id_token = user['idToken']
+      return JSONResponse(status_code=status.HTTP_200_OK,content={"message": "Login successful", "idToken": id_token})
+  except Exception as e:
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail={"message": "Invalid credentials"})
